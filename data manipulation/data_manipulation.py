dba8c5c1d83635c76fd58cc9e97cd0d3cc5e8eb7
athanie2003 <aaravan2003@gmail.com>
2025-11-12 18:18:58 -0500

    Remove empty irradiation and resample to 30min

--- data manipulation/data_manipulation.py
+++ data manipulation/data_manipulation.py
@@ -1,4 +1,4 @@
-import pandas as pd;
+import pandas as pd
 
 
 df1 = pd.read_csv("data manipulation/Plant_2_Generation_Data.csv")
@@ -7,3 +7,26 @@ df2 = pd.read_csv("data manipulation/Plant_2_Weather_Sensor_Data.csv")
 merged_plant2 = pd.merge(df1, df2, on="DATE_TIME", how="inner")
 
 merged_plant2.to_csv("data manipulation/merged_plant2.csv", index=False)
+
+df = pd.read_csv("data manipulation/merged_plant2.csv")
+
+target_column = "IRRADIATION"
+df_cleaned = df[df[target_column] != 0.0]
+
+agg_rules = {
+    'PLANT_ID_x': 'first',
+    'SOURCE_KEY_x': 'first',
+    'PLANT_ID_y': 'first',
+    'SOURCE_KEY_y': 'first',
+    'DC_POWER': 'mean',
+    'AC_POWER': 'mean',
+    'AMBIENT_TEMPERATURE': 'mean',
+    'MODULE_TEMPERATURE': 'mean',
+    'IRRADIATION': 'mean',
+    'DAILY_YIELD': 'last',
+    'TOTAL_YIELD': 'last'
+}
+
+df_cleaned['DATE_TIME'] = pd.to_datetime(df_cleaned['DATE_TIME'])
+df_cleaned = df_cleaned.set_index('DATE_TIME')
+df_30min = df_cleaned.resample('30Min').agg(agg_rules)
